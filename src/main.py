2f1a742d76ca9206bc038869bc221f7599cdb832
Harilal Bhaskaran <harimjf@gmail.com>
2022-09-23 14:05:46 +0000

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -19,12 +19,23 @@ ua=UserAgent()
 headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-language': 'en-IN,en;q=0.9,ml;q=0.8',
+    'cache-control': 'max-age=0',
+    'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-ch-viewport-width': '1920',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'service-worker-navigation-preload': 'true',
+    'upgrade-insecure-requests': '1',
     'user-agent': ua.random,
 }
 
 def analyze_price(logger):
     app = requests.get(URL, headers=headers, proxies=proxies)
-    app = requests.get(URL, headers=headers)
+    #app = requests.get(URL, headers=headers)
     scrapper = BeautifulSoup(app.content, 'html.parser')
 
     #print(scrapper.prettify())
